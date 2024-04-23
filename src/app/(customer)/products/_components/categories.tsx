1df7b7b1843a553c929e0dabb55fb0bbb8f8c0ba
Pritam <pri10145@gmail.com>
2024-04-23 11:25:36 +0530

    filter by category

--- src/app/(customer)/products/_components/categories.tsx
+++ src/app/(customer)/products/_components/categories.tsx
@@ -1,15 +1,14 @@
-// import { Category } from "@prisma/client";
 import { Category } from "@prisma/client";
 import CategoryItem from "./category-item";
 
-// type CategoriesProps = {
-//   items: () => Promise<Category[]>;
-// };
+type CategoriesProps = {
+  items: () => Promise<Category[]>;
+};
 
-const Categories = ({items}: {items: Category[]} ) => {
+const Categories =async ({items}: CategoriesProps ) => {
   return (
     <section className="flex items-center gap-x-4 overflow-x-auto pb-2">
-      {items.map((category) => (
+      {(await items()).map((category: Category) => (
         <CategoryItem
           key={category.id}
           label={category.name}
--- src/app/(customer)/products/page.tsx
+++ src/app/(customer)/products/page.tsx
@@ -1,44 +1,50 @@
+"use client";
+
 import ProductCard, { ProductCardSkeleton } from "@/components/product-card";
 import db from "@/lib/db";
 import { Suspense } from "react";
 import Categories from "./_components/categories";
+import { useSearchParams } from "next/navigation";
+import { Product } from "@prisma/client";
 
-// const getProducts = () => {
-//   return db.product.findMany({
-//     where: { isAvailableForPurchase: true },
-//     orderBy: { name: "asc" },
-//     include: {
-//       category: true,
-//     },
-//   });
-// };
-
-// const getCategories = () => {
-//   return db.category.findMany({
-//     orderBy: {
-//       name: "asc",
-//     },
-//   });
-// };
-
-const ProductsPage = async () => {
-  const products = await db.product.findMany({
-    where: { isAvailableForPurchase: true },
+const getProducts = (categoryId: string): Promise<Product[]> => {
+  return db.product.findMany({
+    where: {
+      isAvailableForPurchase: true,
+      categoryId: categoryId,
+    },
     orderBy: { name: "asc" },
     include: {
       category: true,
     },
   });
+};
 
-  const categories = await db.category.findMany({
+const getCategories = () => {
+  return db.category.findMany({
     orderBy: {
       name: "asc",
     },
   });
+};
+
+function ProductsPage() {
+  // const products = await db.product.findMany({
+  //   where: { isAvailableForPurchase: true },
+  //   orderBy: { name: "asc" },
+  //   include: {
+  //     category: true,
+  //   },
+  // });
+  // const categories = await db.category.findMany({
+  //   orderBy: {
+  //     name: "asc",
+  //   },
+  // });
 
   return (
     <section className="w-full flex flex-col gap-y-12 mt-10 px-3 max-w-6xl mx-auto">
-      <Categories items={categories} />
+      <Categories items={getCategories} />
       <div className="grid grid-cols-2 md:grid-cols-4 gap-3 sm:gap-5 md:gap-3 lg:gap-8">
         <Suspense
           fallback={
@@ -52,13 +58,21 @@ const ProductsPage = async () => {
             </>
           }
         >
-          {products.map((product) => (
-            <ProductCard key={product.id} {...product} />
-          ))}
+          <ProductSuspense />
         </Suspense>
       </div>
     </section>
   );
-};
+}
 
 export default ProductsPage;
+
+async function ProductSuspense() {
+  const searchParams = useSearchParams();
+  const categoryId = searchParams.get("categoryId");
+  const products = getProducts(categoryId as string);
+
+  return (await products).map((product) => (
+    <ProductCard key={product.id} {...product} />
+  ));
+}
